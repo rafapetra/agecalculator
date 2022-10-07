8adf0f442b16cf5dfb079bd1634c919e87eab28b
Rafael Petrachini <raffp@me.com>
2022-10-07 13:05:38 -0700

    Fix first test for Calculator on calculator.test.js

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,8 +4,7 @@ describe('the Calculator and all its prototypes', () => {
 
   test('should correctly create an user object with age, gender and country', () => {
     const calculator = new Calculator(25,79);
-    expect(triangle.userAge).toEqual(25);
-    expect(triangle.userAgeExpectancy).toEqual(79);
-
+    expect(calculator.userAge).toEqual(25);
+    expect(calculator.userAgeExpectancy).toEqual(79);
   });
 });
