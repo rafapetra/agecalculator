2fb7fe363a00abfdce9856ea32a256eebdac7f61
Rafael Petrachini <raffp@me.com>
2022-10-07 13:43:06 -0700

    Pass first test for Calculator

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,8 +1,8 @@
 import Calculator from '../src/calculator.js';
 
-describe('the Calculator and all its prototypes', () => {
+describe('Calculator', () => {
 
-  test('should correctly create an user object with age, gender and country', () => {
+  test('should correctly create an user object with user age and age expectancy', () => {
     const calculator = new Calculator(25,79);
     expect(calculator.userAge).toEqual(25);
     expect(calculator.userAgeExpectancy).toEqual(79);
---
+++ src/calculator.js
@@ -0,0 +1,10 @@
+export default class Calculator {
+  constructor(age, expectedAge){
+    this.userAge = age;
+    this.userAgeExpectancy = expectedAge;
+  }
+
+  checkType() {
+
+  }
+}
