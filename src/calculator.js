45e2d799916b5d2bbb4999e483b582fc49c91327
Rafael Petrachini <raffp@me.com>
2022-10-08 10:56:24 -0700

    Fix class syntax on calculator.js + run build

--- src/calculator.js
+++ src/calculator.js
@@ -1,34 +1,30 @@
 export default class Calculator {
-  constructor(age, expectedAge){
+  constructor(age, expectedAge) {
     this.userAge = age;
     this.userAgeExpectancy = expectedAge;
   }
 
-  inMercury = function() {
-  return this.userAge * .24;
+  inMercury() {
+    return this.userAge * 0.24;
+  }
+
+  inVenus() {
+    return this.userAge * 0.62;
+  }
+
+  inMars() {
+    return this.userAge * 1.88;
+  }
+
+  inJupiter() {
+    return this.userAge * 11.86;
+  }
+
+  yearsLeft() {
+    if (this.userAge < this.userAgeExpectancy) {
+      return this.userAgeExpectancy - this.userAge;
+    } else if (this.userAge > this.userAgeExpectancy) {
+      return this.userAge - this.userAgeExpectancy;
+    }
+  }
 }
-
-  inVenus = function() {
-    return this.userAge * .62;
-}
-
-  inMars = function() {
-  return this.userAge * 1.88;
-}
-
-  inJupiter = function() {
-  return this.userAge * 11.86;
-}
-
-yearsLeft = function() {
-  if ((this.userAge < this.userAgeExpectancy)) {
-    return this.userAgeExpectancy - this.userAge;
-  } else if ((this.userAge > this.userAgeExpectancy))
-    return this.userAge - this.userAgeExpectancy;
-}
-}
-
-
-
-
-
